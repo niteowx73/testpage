e24b221424a1853a2083629e074debc3d3fa3879
niteowx73 <137158492+niteowx73@users.noreply.github.com>
2025-01-25 16:26:12 +0200

    Delete script.js
    del

--- script.js
+++
@@ -1,9 +0,0 @@
-// Оригінальний код JavaScript
-
-const openInChrome = () => {
-  const url = 'https://www.wikipedia.org'; // Заміна на потрібне посилання
-  window.location.href = `googlechrome://${url.replace(/^https?:\/\//, '')}`;
-};
-
-// Додайте цей код в HTML файл:
-document.getElementById('open-in-chrome').addEventListener('click', openInChrome);
